4709b393c362533e8f8fc5783813c45fd6dc77ad
Gatien Chenu <gatien+dev@chenu.me>
2024-05-20 14:25:19 +0200

    force ozone wl

--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -2,4 +2,7 @@
 
 	programs.hyprland.enable = true;
 	programs.hyprland.package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+
+	environment.sessionVariables = { NIXOS_OZONE_WL = "1"; };
+	
 }
