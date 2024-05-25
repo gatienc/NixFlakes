5fda1a5eb544b013c46d9b711b1a9e2338356041
Gatien Chenu <gatien+dev@chenu.me>
2024-05-25 16:46:05 +0200

    tried to remove initial background

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -164,6 +164,13 @@
         ", XF86MonBrightnessDown, exec, brightnessctl s 5%-"
       ];
 
+      misc = {
+        # See https://wiki.hyprland.org/Configuring/Variables/ for more
+        force_default_wallpaper =-1; # Set to 0 to disable the anime mascot wallpapers
+        disable_hyprland_logo = true;
+        force_hypr_chan = false;
+      };
+
       exec-once = ''${startupScript}/bin/start'';
       
     };
