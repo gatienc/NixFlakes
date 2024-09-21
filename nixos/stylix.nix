3bdba2a46a91c918e0b4e875ce547a4007db9ebf
Gatien Chenu <gatien+dev@chenu.me>
2024-09-22 00:54:46 +0200

    some change for stylix

--- nixos/stylix.nix
+++ nixos/stylix.nix
@@ -1,33 +1,43 @@
-{ pkgs,lib, ... }: 
+{ pkgs, lib, ... }:
 {
-      stylix.cursor.package = pkgs.bibata-cursors;
-      stylix.cursor.name = "Bibata-Modern-Ice";
-      stylix.cursor.size = 12;
-      stylix.image = ../assets/wallpaper/pixel_kanagawa.png;
-      stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
-      stylix.fonts = {
-            monospace = {
-                  package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
-                  name = "JetBrainsMono Nerd Font Mono";
-            };
-            sansSerif = {
-                  package = pkgs.dejavu_fonts;
-                  name = "DejaVu Sans";
-            };
-            serif = {
-                  package = pkgs.dejavu_fonts;
-                  name = "DejaVu Serif";
-            };
-      };
+  stylix = {
+    enable = true;
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 16;
+    };
 
-      stylix.opacity = {
-            applications = 0.95;
-            terminal = 0.8;
-            desktop = 1.0;
-            popups = 0.95;
+    image = ../assets/wallpaper/pixel_kanagawa.png;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+    fonts = {
+      monospace = {
+        package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
+        name = "JetBrainsMono Nerd Font Mono";
       };
-      
-      stylix.polarity = "dark"; # "light" or "either"
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+      emoji = {
+        package = pkgs.whatsapp-emoji-linux;
+        name = "WhatsApp Emoji";
+      };
+    };
+
+    opacity = {
+      applications = 0.95;
+      terminal = 0.8;
+      desktop = 1.0;
+      popups = 0.95;
+    };
+
+    polarity = "dark"; # "light" or "either"
+  };
 
 }
 
