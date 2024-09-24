cb9b14fbf9a56bc4e0a47aa3acd5b4eb599f7012
Gatien Chenu <gatien+dev@chenu.me>
2024-09-25 01:15:24 +0200

    adding minecraft port as open port

--- nixos/minecraft.nix
+++ nixos/minecraft.nix
@@ -1,6 +1,10 @@
 { pkgs, lib, ... }:
 
 {
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 25565 ];
+  };
   services.minecraft-server = {
     enable = true;
     eula = true;
@@ -17,9 +21,11 @@
     };
 
     whitelist = {
-      vimjoyer = "3f96da12-a55c-4871-8e26-09256adac319";
+      teyzen = "b3d86a49-f7bc-4bc8-80be-45163719d0af"
+
+        };
+
+      jvmOpts = "-Xms4092M -Xmx4092M -XX:+UseG1GC";
     };
 
-    jvmOpts = "-Xms4092M -Xmx4092M -XX:+UseG1GC";
-  };
-}
+  }
