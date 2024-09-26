d0696bbce0179cdb6f0e918d7f0c7b01a3928915
Gatien Chenu <gatien+dev@chenu.me>
2024-09-26 22:49:32 +0200

    enabling gnome

--- nixos/remote-desktop.nix
+++ nixos/remote-desktop.nix
@@ -5,28 +5,11 @@
     autoStart = true;
     capSysAdmin = true;
     openFirewall = true;
-
   };
+
   services.xserver = {
     enable = true;
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
   };
-  environment.systemPackages = [ pkgs.gnome-session ];
-  services.xrdp.enable = true;
-  services.xrdp.defaultWindowManager = "gnome-session";
-  services.xrdp.openFirewall = true;
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 3389 ];
-  };
-
-  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
-  # If no user is logged in, the machine will power down after 20 minutes.
-  systemd.targets.sleep.enable = false;
-  systemd.targets.suspend.enable = false;
-  systemd.targets.hibernate.enable = false;
-  systemd.targets.hybrid-sleep.enable = false;
-
-
 }
