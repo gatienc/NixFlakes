650e429a2a691565a16aabbf7e8ca62b1065eb31
Gatien    Chenu <gatien + dev@chenu.me>
2025-06-01 21:08:46 +0200

    fix: pkgs.gnome.gnome-session deprecated for pkgs.gnome-session

--- modules/core/remote-desktop.nix
+++ modules/core/remote-desktop.nix
@@ -9,7 +9,7 @@
   services.xrdp = {
     enable = true;
     package = pkgs.xrdp;
-    defaultWindowManager = "${pkgs.gnome.gnome-session}/bin/gnome-session";
+    defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
     openFirewall = true;
   };
 }
