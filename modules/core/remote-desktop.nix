27598f726ebd5a8de5c3963f0892dc27900ad692
Gatien    Chenu <gatien + dev@chenu.me>
2025-06-02 18:55:48 +0200

    fix: disable GNOME3/GDM auto-suspend features to prevent unintended power downs

--- modules/core/remote-desktop.nix
+++ modules/core/remote-desktop.nix
@@ -12,4 +12,12 @@
     defaultWindowManager = "${pkgs.gnome-remote-desktop}/bin/gnome-remote-desktop";
     openFirewall = true;
   };
+
+  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
+  # If no user is logged in, the machine will power down after 20 minutes.
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
 }
