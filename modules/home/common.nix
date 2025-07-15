14914efab94187c8641571103e509bab9a76575f
Gatien Chenu <gatien+dev@chenu.me>
2025-07-15 17:19:56 +0200

    m

--- modules/home/common.nix
+++ modules/home/common.nix
@@ -1,3 +1,4 @@
+
 {
   inputs,
   pkgs,
@@ -6,9 +7,9 @@
   username,
   ...
 }:
-{
-
-  home.packages = with pkgs; [
+let
+  # Common packages for all systems
+  commonPackages = with pkgs; [
     # Terminal
     bat # replacement for cat
     bat-extras.batdiff # extra features for bat
@@ -20,14 +21,9 @@
     zellij # tmux alternative
     lazygit # git terminal
     lazydocker # docker terminal
-    bluetuith # bluetooth manager
-    clipse # clipboard manager
     nmap # network exploration tool and security/port scanner
     arp-scan # network discovery tool
 
-    # tlock # 2fa tui
-    # calcure # calendar tui
-    gsimplecal # calendar
     tealdeer # tldr client
     wget # download files from the web
     ranger # terminal file manager
@@ -54,22 +50,6 @@
     gh-dash # github dashboard
     copier # project templating tool
 
-    # Desktop environment
-    swww # wallpaper manager
-    fuzzel # launcher
-    rofi-wayland # application launcher
-    dunst # notification manager
-    libnotify # notification tool
-
-    cava # audio visualizer
-    iniparser # for cava
-
-    alsa-utils # ALSA sound utilities
-
-    # Office software
-    libreoffice-qt # office suite
-    hunspell # spell checker
-
     # Nix
     nixfmt-rfc-style # nix formatting tool
     direnv # environment variable manager
@@ -87,20 +67,14 @@
     dysk # disk usage analyzer
     iotop # io monitoring
     iftop # network monitoring
-    radeontop # GPU monitoring
 
-    # Display image TODO: select one
+    # Display image
     timg
     gthumb
     w3m
 
     ffmpeg
-    # media playing
     playerctl # control media players
-    brightnessctl # control screen brightness
-    pavucontrol # control audio devices
-    pulsemixer # control audio devices
-    pamixer # control audio volume
 
     # fun
     cowsay
@@ -109,74 +83,137 @@
     figlet # text art generator
     cmatrix # terminal screensaver
     asciiquarium # aquarium in terminal
-
   ];
 
-  programs = {
-    home-manager.enable = true;
-    git = {
-      enable = true;
-      lfs.enable = true;
-      userName = "\n    Gatien\n    Chenu ";
-      userEmail = "\n    gatien + dev@chenu.me";
-    };
-    firefox.enable = true;
+  # Linux-only packages
+  linuxPackages = with pkgs; [
+    bluetuith # bluetooth manager
+    clipse # clipboard manager
+    gsimplecal # calendar
+    # Desktop environment
+    swww # wallpaper manager
+    fuzzel # launcher
+    rofi-wayland # application launcher
+    dunst # notification manager
+    libnotify # notification tool
 
-    fzf.enable = true; # enables zsh integration by default
-    starship.enable = true;
-    kitty = {
-      enable = true;
-      keybindings = {
-        "ctrl+alt+f" =
-          "launch --type=overlay --stdin-source=@screen_scrollback ${pkgs.fzf}/bin/fzf --no-sort --no-mouse --exact -i";
-      };
-    };
-    zsh = {
-      enable = true;
-      enableCompletion = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
-      shellAliases = {
-        nixswitch = "sudo nixos-rebuild switch";
-        cd = "z";
-        nano = "micro";
-        cat = "bat";
-        icat = "kitty +kitten icat";
-        grep = "rg";
-        diff = "batdiff";
-        man = "batman";
-        ls = "eza --icons --group-directories-first";
-        ll = "eza --icons -l --group-directories-first";
-        tree = "eza --tree --icons";
-        clip = "wl-copy";
-        whatismyip = "curl https://ipinfo.io/ip";
-        logout = "hyprctl dispatch exit";
-        c = "clear";
-        g = "lazygit";
-        gc = ''git commit -m "$1"'';
+    cava # audio visualizer
+    iniparser # for cava
 
-      };
+    alsa-utils # ALSA sound utilities
 
-      oh-my-zsh = {
+    # Office software
+    libreoffice-qt # office suite
+    hunspell # spell checker
+
+    radeontop # GPU monitoring
+    brightnessctl # control screen brightness
+    pavucontrol # control audio devices
+    pulsemixer # control audio devices
+    pamixer # control audio volume
+  ];
+
+in
+lib.mkMerge [
+  # ===================================================================
+  #
+  #                            Common Config
+  #
+  # ===================================================================
+  {
+    home.packages = commonPackages;
+
+    programs = {
+      home-manager.enable = true;
+      git = {
         enable = true;
-        plugins = [
-          "git"
-          "node"
-          "npm"
-        ];
-        theme = "af-magic";
+        lfs.enable = true;
+        userName = "Gatien Chenu";
+        userEmail = "gatien+dev@chenu.me";
+      };
+      firefox.enable = true;
+
+      fzf.enable = true; # enables zsh integration by default
+      starship.enable = true;
+      kitty = {
+        enable = true;
+        keybindings = {
+          "ctrl+alt+f" =
+            "launch --type=overlay --stdin-source=@screen_scrollback ${pkgs.fzf}/bin/fzf --no-sort --no-mouse --exact -i";
+        };
+      };
+      zsh = {
+        enable = true;
+        enableCompletion = true;
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
+        shellAliases = {
+          cd = "z";
+          nano = "micro";
+          cat = "bat";
+          icat = "kitty +kitten icat";
+          grep = "rg";
+          diff = "batdiff";
+          man = "batman";
+          ls = "eza --icons --group-directories-first";
+          ll = "eza --icons -l --group-directories-first";
+          tree = "eza --tree --icons";
+          whatismyip = "curl https://ipinfo.io/ip";
+          c = "clear";
+          g = "lazygit";
+          gc = ''git commit -m "$1"'';
+        };
+
+        oh-my-zsh = {
+          enable = true;
+          plugins = [
+            "git"
+            "node"
+            "npm"
+          ];
+          theme = "af-magic";
+        };
+      };
+
+      zoxide = {
+        enable = true;
+        enableZshIntegration = true;
       };
     };
 
-    zoxide = {
-      enable = true;
-      enableZshIntegration = true;
+    # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+    home.stateVersion = "24.05";
+  }
+
+  # ===================================================================
+  #
+  #                            Linux Config
+  #
+  # ===================================================================
+  (lib.mkIf pkgs.stdenv.isLinux {
+    home.packages = linuxPackages;
+
+    programs.zsh.shellAliases = {
+      nixswitch = "sudo nixos-rebuild switch";
+      logout = "hyprctl dispatch exit";
+      clip = "wl-copy";
     };
-  };
+  })
 
-  # Nicely reload system units when changing configs
-  #systemd.user.startServices = "sd-switch";
+  # ===================================================================
+  #
+  #                            macOS Config
+  #
+  # ===================================================================
+  (lib.mkIf pkgs.stdenv.isDarwin {
+    # Add any macOS-specific packages here
+    # home.packages = [ pkgs.mac-app-utils ];
 
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "23.11";
-}
+    programs.zsh.shellAliases = {
+      # Example of a macOS-specific alias
+      # nixswitch = "darwin-rebuild switch --flake ~/NixFlakes";
+      clip = "pbcopy";
+    };
+  })
+]
+
