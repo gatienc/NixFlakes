1cb34f3599ac82bab4c358f73baf827560c4258d
Gatien    Chenu <gatien + dev@chenu.me>
2025-07-15 17:03:53 +0200

    m

--- flake.nix
+++ flake.nix
@@ -104,7 +104,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.noghartt = import ./home/home.nix;
+              # home-manager.users.noghartt = import ./home/home.nix;
             }
           ];
         };
