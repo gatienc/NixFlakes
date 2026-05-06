0187ed65aa58ebdddc7099f66a2eecc0b54a4713
Gatien Chenu <gatien+dev@chenu.me>
2026-05-06 10:17:56 +0200

    trying system wide python fix for geospatial library and more : WIP

--- modules/core/python.nix
+++ modules/core/python.nix
@@ -1,8 +1,32 @@
 { pkgs, ... }:
 {
+  # Enable nix-ld for dynamically linked binaries
+  programs.nix-ld.enable = true;
+
+  # Libraries available system-wide for dynamically linked executables
   programs.nix-ld.libraries = with pkgs; [
     stdenv.cc.cc.lib
     zlib
     expat
+    gdal
+    proj
+    geos
+    libxml2
+    libspatialite
+    sqlite
+    libtiff
+    libjpeg
+    libpng
+    openssl
+    curl
+    hdf5
+    netcdf
   ];
+
+  # Environment variables for Python geospatial packages
+  environment.sessionVariables = {
+    GDAL_LIBRARY_PATH = "${pkgs.gdal}/lib/libgdal.so";
+    GEOS_LIBRARY_PATH = "${pkgs.geos}/lib/libgeos_c.so";
+    PROJ_LIB = "${pkgs.proj}/share/proj";
+  };
 }
